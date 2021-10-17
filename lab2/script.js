da51fe800c828387f8fc204bcaa1fe00d9b6d0b7
Marichka <marichka.v.richtsi@gmail.com>
2021-10-17 23:13:33 +0300

    fix failed to fetch error

--- lab2/script.js
+++ lab2/script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('mailForm');
 
 function sendRequest(data) {
-	const response = fetch('https://lab2apiemail.azurewebsites.net/email', {
+	fetch('https://lab2apiemail.azurewebsites.net/email', {
 		method: 'POST',
 		mode: 'cors',
 		cache: 'no-cache',
@@ -12,28 +12,32 @@ function sendRequest(data) {
 		redirect: 'follow',
 		referrerPolicy: 'no-referrer',
 		body: JSON.stringify(data),
-	});
+	}).then(response => response.json())
+		.then(result => {
+			if (result.status === 400) {
+				const { errors } = result;
+				const errorsList = [];
 
-	if (response.status === 400) {
-		const { errors } = response.json();
-		const errorsList = [];
+				for (const key in errors) {
+					for (const error of errors[key]) {
+						errorsList.push(error);
+					}
+				}
 
-		for (const key in errors) {
-			for (const error of errors[key]) {
-				errorsList.push(error);
+				const errorsString = errorsList.join('\n');
+				alert(`Error ${result.status}:\n${errorsString}`);
+				document.getElementById('spinner').style.display = 'none';
+			} else if (result.status < 200 || result.status > 400) {
+				alert(`Error ${result.status}`);
+				document.getElementById('spinner').style.display = 'none';
+			} else if (result.status >= 200 || result.status < 300) {
+				alert(`Done!`);
+				document.getElementById('spinner').style.display = 'none';
 			}
-		}
-
-		const errorsString = errorsList.join('\n');
-		alert(`Error ${response.status}:\n${errorsString}`);
-		document.getElementById('spinner').style.display = 'none';
-	} else if (response.status < 200 || response.status > 400) {
-		alert(`Error ${response.status}`);
-		document.getElementById('spinner').style.display = 'none';
-	} else if (response.status >= 200 || response.status < 300) {
-		alert(`Done!`);
-		document.getElementById('spinner').style.display = 'none';
-	}
+		}).catch(error => {
+			alert(`Error ${error}`);
+			document.getElementById('spinner').style.display = 'none';
+		});
 }
 
 function retriveFormValue(event) {
