0a88c6fdb84a12e6db0d2de5d3674c5395aad13b
Marichka <marichka.v.richtsi@gmail.com>
2021-11-24 19:26:07 +0200

    fix configuration

--- lab2/WebAppLab2/WebAppLab2/Startup.cs
+++ lab2/WebAppLab2/WebAppLab2/Startup.cs
@@ -21,11 +21,11 @@ namespace WebAppLab2
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            Origins = configuration["CORS Origins"].Split(';').ToArray();
+            Origins = configuration["CORS Origins"];
         }
 
         public IConfiguration Configuration { get; }
-        public string[] Origins { get; }
+        public string Origins { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
